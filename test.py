c9121dab383931f8bff85293df4d304e5cbff06d
“aboutmydreams <aboutmydreams@163.com>
2019-01-15 22:58:46 +0800

    some condition

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ da = {
     'E2532': 'The two authentication interval cannot be less than 10 seconds',
     }
 
-url_list = ['https://www.baidu.com','https://zhidao.baidu.com','https://hanyu.baidu.com', 'http://www.kugou.com','https://www.sina.com.cn/','https://weibo.com/','http://www.sohu.com/','http://site.baidu.com/','https://www.guazi.com','https://open.163.com/','https://www.autohome.com.cn','https://www.imooc.com/','https://modao.cc/','https://jusp.tmall.com','http://www.4399.com/','http://www.tuniu.com/','https://mobile.pconline.com.cn/','http://www.rayli.com.cn/','http://www.hao123.com/zxfy','http://cp.iciba.com/']
+url_list = ['https://www.cnblogs.com/','https://www.coolapk.com/','https://www.w3cschool.cn/','https://www.baidu.com','https://zhidao.baidu.com','https://hanyu.baidu.com', 'http://www.kugou.com','https://www.sina.com.cn/','https://weibo.com/','http://www.sohu.com/','http://site.baidu.com/','https://www.guazi.com','https://open.163.com/','https://www.autohome.com.cn','https://www.imooc.com/','https://modao.cc/','https://jusp.tmall.com','http://www.4399.com/','http://www.tuniu.com/','https://mobile.pconline.com.cn/','http://www.rayli.com.cn/','http://www.hao123.com/zxfy','http://cp.iciba.com/']
 
 t1 = time.time()
 def ncuwlan(name,psw):
@@ -35,30 +35,34 @@ def ncuwlan(name,psw):
         res = requests.post(url,data=data,timeout=5).text
         if 'login_ok' in res:
             print("connect!")
+            return 'ok'
         elif 'E2532' in res:
             print('rest some time')
             time.sleep(12)
+            return 'ok'
+        elif ('E2531' in res) or ('E2553' in res):
+            return 'wrong'
+        elif 'Arrearage' in res:
+            return 'far'
         else:
             print('something wrong')
             print(res[0:5] + str(da))
     except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError,requests.exceptions.ReadTimeout):
         print('ncuwlan can not be connected becuuse timeout')
-    finally:
-        pass
-        return t1
+        return 'timeout'
 
 def connect(name,psw):
     while True:
-        aurl = choice(url_list)
+        a_url = choice(url_list)
         try:
-            now_res = requests.get(aurl,timeout=10)
+            now_res = requests.get(a_url,timeout=10)
             # print(now_res.status_code)
             time.sleep(7)
         except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError,requests.exceptions.ReadTimeout):
             t1 = ncuwlan(name,psw)
             t2 = time.time()
             t3 = time.ctime()
-            print('{}s connected with{},now time is {}'.format(str(round(t2-t1,2)),t3,aurl))
+            print('{}s connected with{},now time is {}'.format(str(round(t2-t1,2)),t3,a_url))
 
 
 class login(QWidget):
@@ -95,9 +99,21 @@ class login(QWidget):
         psw = self.pswLineEdit.text()
         print('name: %s psw: %s ' % (name,psw))
         self.alert = QMessageBox()
-        self.alert.setText('登录成功')
+        self.alert.setText('123133213登录成功')
         self.alert.exec_()
 
+        if ncuwlan(name,psw) == 'ok':
+            self.alert = QMessageBox()
+            self.alert.setText('登录成功')
+            self.alert.exec_()
+        elif ncuwlan(name,psw) == 'wrong':
+            self.alert = QMessageBox()
+            self.alert.setText('用户名或密码错了噢')
+            self.alert.exec_()
+        elif ncuwlan(name,psw) == 'far':
+            self.alert = QMessageBox()
+            self.alert.setText('超过范围了噢')
+            self.alert.exec_()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = login()
